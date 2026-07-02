2f4679d612365dc719b190d5530c2478e79be15e
Royer888 <rogerfloreschoque8@gmail.com>
2026-07-02 17:36:09 -0400

    proyecto base GRUPO 3

---
+++ src/main/java/com/example/ExamenFinal_Grupo3/ExamenFinalGrupo3Application.java
@@ -0,0 +1,13 @@
+package com.example.ExamenFinal_Grupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalGrupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalGrupo3Application.class, args);
+	}
+
+}
